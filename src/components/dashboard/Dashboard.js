45e535a434e38a43eb6970831701ae599942a60c
augustofb <augusto.brandao@presidencia.gov.br>
2019-11-26 13:33:24 -0300

    Layout da dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Paper, Container } from '@material-ui/core';
 
 const useStyles = makeStyles(theme =>( {
     container: {
-      width: '100vw',
+      maxWidth: '100vw',
+      paddingTop: '2rem'
     },
+    rightGrid: {
+        height: '90vh'
+    },
+    lefttGrid: {
+        height: '45vh'
+    }
   }));
 
 export default function Dashboard() {
@@ -13,25 +20,28 @@ export default function Dashboard() {
     const classes = useStyles();
 
     return(
-            <Grid container className={classes.container}>
+        <Container className={classes.container}>
+            <Grid container spacing={5}>
                 <Grid item xs={6}>
-                    <Grid container>
-                    <Grid item xs={12}>
-                        <Typography>
-                            Estatísticas
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}></Grid>
-                        <Typography>
-                            Previsões
-                        </Typography>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} className={classes.lefttGrid} component={Paper} color={'primary'}>
+                            <Typography>
+                                Estatísticas
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} className={classes.lefttGrid} component={Paper}>
+                            <Typography>
+                                Previsões
+                            </Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={6} className={classes.rightGrid} component={Paper}>
                     <Typography>
                         Ações
                     </Typography>
                 </Grid>
             </Grid>
+        </Container>
     )
 }
