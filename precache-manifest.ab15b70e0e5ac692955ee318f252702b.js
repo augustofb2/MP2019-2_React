9a46a069d9420b65937035ddc890349fd95cd07b
augustofb2 <augustofb2@gmail.com>
2019-12-07 21:35:30 -0300

    Updates

---
+++ precache-manifest.ab15b70e0e5ac692955ee318f252702b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7fe42eeef380683fb34468e57e2518",
+    "url": "/MP2019-2_React/index.html"
+  },
+  {
+    "revision": "70d4ccf294f894269c5b",
+    "url": "/MP2019-2_React/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "34c980ba4b5e8c31d332",
+    "url": "/MP2019-2_React/static/js/2.1c3e482a.chunk.js"
+  },
+  {
+    "revision": "70d4ccf294f894269c5b",
+    "url": "/MP2019-2_React/static/js/main.037d66f2.chunk.js"
+  },
+  {
+    "revision": "b2943ef8e908d3f3554b",
+    "url": "/MP2019-2_React/static/js/runtime-main.6b6672e1.js"
+  }
+]);
